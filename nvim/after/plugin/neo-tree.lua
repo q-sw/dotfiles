35425b5ee5aa89de3d220faa287c834282c72790
q-sw <quentin.swiec@gmail.com>
2023-12-16 19:02:53 +0100

    [UPDATE] auto close pane

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -30,5 +30,18 @@ require("neo-tree").setup({
                 --"node_modules",
             },
         },
+    },
+    event_handlers = {
+
+        {
+            event = "file_opened",
+            handler = function(file_path)
+                -- auto close
+                -- vimc.cmd("Neotree close")
+                -- OR
+                require("neo-tree.command").execute({ action = "close" })
+            end
+        },
+
     }
 })
