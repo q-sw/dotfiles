93a158f42b5ad3b0eb920abed18817ce101864c4
q-sw <quentin.swiec@gmail.com>
2023-12-16 19:02:31 +0100

    [UPDATE] add shortcut to get help in insert mode

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -55,6 +55,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 end)
 
