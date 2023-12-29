db265984c5446a864d17a0b2a009d4b8b7cd0e51
q-sw <quentin.swiec@gmail.com>
2023-12-29 15:00:10 +0100

    [UPDATE] add Git commit shortcut

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 
-vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-&>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-é>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-à>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-ç>", function() ui.nav_file(4) end)
