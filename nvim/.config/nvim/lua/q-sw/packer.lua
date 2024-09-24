7329597679dae95f97cd0c8604c54ba178db88b5
Quentin SWIECH <quentin.swiech@s3ns.io>
2024-09-24 16:56:55 +0200

    [update] add cmp plugins

--- nvim/.config/nvim/lua/q-sw/packer.lua
+++ nvim/.config/nvim/lua/q-sw/packer.lua
@@ -27,6 +27,9 @@ return require("packer").startup(function(use)
 			{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
 
 			-- AUTO COMPLETE
+			--
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lua" },
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "hrsh7th/cmp-buffer" },
 			{ "hrsh7th/cmp-path" },
@@ -36,6 +39,8 @@ return require("packer").startup(function(use)
 			-- SNIPPSET
 			{ "hrsh7th/cmp-vsnip" },
 			{ "hrsh7th/vim-vsnip" },
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
 		},
 	})
 
