18d9ad4949417627c9293141d65745b778fd1baf
Quentin SWIECH <quentin.swiech@s3ns.io>
2024-09-28 08:06:49 +0200

    [add] script to manage my displays

--- i3/.config/i3/screen_manager.sh
+++ i3/.config/i3/screen_manager.sh
@@ -4,9 +4,9 @@ choices="laptop\noffice-dual\noffice-mono\nhome\nhome-work"
 chosen=$(echo -e $choices | dmenu -i)
 
 case "$chosen" in
-    laptop) xrandr --output eDP-1 --auto --primary --output HDMI-1 --off --output DP-3 --off ;;
-    office-mono) xrandr --output eDP-1 --off --output DP-3 --auto --primary ;;
-    office-dual) xrandr --output eDP-1 --auto --primary --output DP-3 --auto --above eDP-1 ;;
-    home) xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-1 --rotate right;;
-    home-work) xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-3 --auto --right-of HDMI-1 --rotate right;;
+    laptop) xrandr --output eDP-1 --auto --primary --output HDMI-1 --off --output DP-3 --off && setxkbmap fr ;;
+    office-mono) xrandr --output eDP-1 --off --output DP-3 --auto --primary && setxkbmap fr;;
+    office-dual) xrandr --output eDP-1 --auto --primary --output DP-3 --auto --above eDP-1 && setxkbmap fr;;
+    home) xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-1 --rotate right && setxkbmap us;;
+    home-work) xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-3 --auto --right-of HDMI-1 --rotate right && setxkbmap us;;
 esac
